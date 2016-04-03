1a78afbb908dc5d491e4134d215b270ac8ea0887
Paul Davies <paul_t_d@hotmail.com>
2016-04-03 12:50:22 +0100

    Moving over to new project

---
+++ Campervibe-domain/src/uk/co/luciditysoftware/campervibe/domain/requests/booking/MakeRequest.java
@@ -0,0 +1,42 @@
+package uk.co.luciditysoftware.campervibe.domain.requests.booking;
+
+import java.util.*;
+
+import uk.co.luciditysoftware.campervibe.domain.entities.Vehicle;
+
+public class MakeRequest {
+	private Date startDate;
+	private Date endDate;
+	private Vehicle vehicle;
+	//private virtual Customer Customer { get; set; }
+	
+	public Date getStartDate() 
+	{
+		return startDate;
+	}
+	
+	public void setStartDate(Date startDate) 
+	{
+		this.startDate = startDate;
+	}
+	
+	public Date getEndDate() 
+	{
+		return endDate;
+	}
+	
+	public void setEndDate(Date endDate) 
+	{
+		this.endDate = endDate;
+	}
+	
+	public Vehicle getVehicle() 
+	{
+		return vehicle;
+	}
+	
+	public void setVehicle(Vehicle vehicle) 
+	{
+		this.vehicle = vehicle;
+	}
+}
